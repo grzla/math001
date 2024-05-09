213107b9ba4a1e365db74e921a2bbb80a5e2c8f6
grzla <greg.j.lundberg@gmail.com>
2024-05-08 20:25:44 -0400

    added logging detail

--- math001.js
+++ math001.js
@@ -1,11 +1,10 @@
 // The html file will ask the user for two numbers.Your program will multiply the two numbers and output the result to the webpage
 
-
 multiply = (num1, num2) => {
     event.preventDefault()
 
-    console.log(typeof num1, typeof num2)
-    console.log(typeof num1.value, typeof num2.value)
+    console.log('Types of num1 and num2: ' + typeof num1, typeof num2)
+    console.log('Types of num1.value and num2.value: ' + typeof num1.value, typeof num2.value)
 
     let resultEl = document.getElementById("result")
 
