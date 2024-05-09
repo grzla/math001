2382d8d849e4e8c08c0239da6e3f9bdf92abe3c6
grzla <greg.j.lundberg@gmail.com>
2024-05-08 20:18:23 -0400

    calling multiply from from with num args

--- math001.js
+++ math001.js
@@ -1,15 +1,13 @@
 // The html file will ask the user for two numbers.Your program will multiply the two numbers and output the result to the webpage
 
-// let num1 = parseInt(document.getElementById("num1").value)
-// let num2 = parseInt(document.getElementById("num2").value)
-
-let num1, num2;
-
-let resultEl = document.getElementById("result")
 
 multiply = (num1, num2) => {
     event.preventDefault()
-    num1 = parseInt(document.getElementById("num1").value)
-    num2 = parseInt(document.getElementById("num2").value)
-    resultEl.innerText = num1 * num2
+
+    console.log(typeof num1, typeof num2)
+    console.log(typeof num1.value, typeof num2.value)
+
+    let resultEl = document.getElementById("result")
+
+    resultEl.innerText = num1.value * num2.value
 }
